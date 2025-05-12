3de2ff8410215c1b08234220a876c9ee393e9428
Breysisalazar <165910652+Breysisalazar@users.noreply.github.com>
2025-05-11 22:57:02 -0500

    Add files via upload

--- ingesta_compras.py
+++ ingesta_compras.py
@@ -1,40 +1,46 @@
 import pandas as pd
 import pymysql
 import boto3
-print("Paso 1 completado")
+import logging
+import os
 
-# Conexion a la base de datos MySQL
-conn = pymysql.connect(
-    host='mysql_c',              # Nombre del contenedor de MySQL
-    port=3306,                   # Puerto del contenedor MySQL
-    user='root',                 # Usuario de MySQL
-    password='utec',             # Contrasena de MySQL
-    database='compras_db'        # Nombre de la base de datos
+# Configurar logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
 )
-print("Paso 2 completado")
+try:
+    logging.info("Paso 1: Conectando a la base de datos")
+    conn = pymysql.connect(
+        host='mysql_c',
+        port=3306,
+        user='root',
+        password='utec',
+        database='compras_db'
+    )
+    logging.info("Conexion exitosa a MySQL")
+    query = """
+    SELECT p.id_pasajero, p.nombre_completo, p.email, p.fecha_nacimiento, p.telefono, m.tipo,
+           c.id_compra, c.fecha, c.asiento, m.fecha_exploracion
+    FROM Pasajero p
+    JOIN Membresia m ON p.id_pasajero = m.id_pasajero
+    JOIN Compra c ON p.id_pasajero = c.id_pasajero
+    """
+    df = pd.read_sql(query, conn)
+    logging.info(f"Consulta ejecutada correctamente. Registros obtenidos: {len(df)}")
+# Guardar como CSV
+    csv_file = "compras.csv"
+    df.to_csv(csv_file, index=False)
+    logging.info(f"Archivo CSV guardado como {csv_file}")
+# Subir a S3
+    s3 = boto3.client('s3')
+    s3.upload_file(csv_file, "ingesta-datos", "ingesta01/compras.csv")
+    logging.info("Archivo CSV subido exitosamente a S3")
+dpx-gwsd-wgn
 
-# Leer los datos con una consulta SQL
-query = """
-SELECT p.id_pasajero, p.nombre_completo, p.email, p.fecha_nacimiento, p.telefono, m.tipo,
-       c.id_compra, c.fecha, c.asiento, m.fecha_exploracion
-FROM Pasajero p
-JOIN Membresia m ON p.id_pasajero = m.id_pasajero
-JOIN Compra c ON p.id_pasajero = c.id_pasajero
-"""
-df = pd.read_sql(query, conn)
-print("Paso 3 completado")
-
-# Cerrar la conexion
-conn.close()
-print("Paso 3 completado")
-
-# Guardar los datos como un archivo CSV
-csv_file = "compras.csv"
-df.to_csv(csv_file, index=False)
-print("Paso 4 completado")
-
-# Subir el archivo CSV a S3
-s3 = boto3.client('s3')
-s3.upload_file(csv_file, "ingesta-datos", "ingesta01/compras.csv")
-
-print("Ingesta completada desde MySQL")
+except Exception as e:
+    logging.error("Ocurrio un error durante la ejecución", exc_info=True)
+finally:
+    if 'conn' in locals() and conn.open:
+        conn.close()
+        logging.info("Conexion a la base de datos cerrada")
